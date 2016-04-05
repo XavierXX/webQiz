b3f744d464831095138cf9d2243023c6ecda7704
ncfxy <359419163@qq.com>
2016-04-05 10:44:37 +0800

    修改加入控件后，布局出错，以及新加的布局控件无法添加新控件的问题

--- public/scripts/my_new_scripts.js
+++ public/scripts/my_new_scripts.js
@@ -1,13 +1,6 @@
 $(document).ready(function() {
 	
-	$(".draggable-control-frame .single-control").draggable({
-		connectToSortable: " .demo, .column",
-		helper: "clone"});
-	$(".demo, .column").sortable({
-		// 允许加入该容器的元素再拖动到其他相关联的容器
-		connectWith: ".demo,.column",
-		opacity: .35
-	});
+	configurationElm();
 
 	// 点击size按钮事件处理函数
 	$("#change-size-btn").click(function(e) {
@@ -27,7 +20,20 @@ $(document).ready(function() {
 function configurationElm(e, t) {
 	$(".draggable-control-frame .single-control").draggable({
 		connectToSortable: " .demo, .column",
-		helper: "clone"});
+		helper: "clone",
+		stop: function(e,t){
+			$(".demo, .column").sortable({
+			// 允许加入该容器的元素再拖动到其他相关联的容器
+			connectWith: ".demo,.column",
+			opacity: .35
+			});
+		}
+	});
+	$(".demo, .column").sortable({
+		// 允许加入该容器的元素再拖动到其他相关联的容器
+		connectWith: ".demo,.column",
+		opacity: .35
+	});
 	
 	console.log($(".draggable-control-frame  .single-control"));
 }
